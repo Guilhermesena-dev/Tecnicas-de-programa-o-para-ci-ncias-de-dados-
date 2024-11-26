677d9b67fc4eb0821c43a6abeb5a33c72a24a854
Guilhermesena-dev <guiisena2022@gmail.com>
2024-11-26 12:15:21 -0300

    Calculadora

---
+++ calculadora.py
@@ -0,0 +1,17 @@
+class memoria:
+    def init_
+
+
+    def soma(a, b):
+
+        return a + b
+    def subtrai(a,b ):
+        return a - b
+    def multiplica(a,b  ):
+        return a * b 
+    def divisão(a,b ):
+        return a*b
+    def _divisão(a,b ):
+        return a/b 
+ 
+
---
+++ testecalculadora.py
@@ -0,0 +1,12 @@
+import calculadora as calc
+
+def testa_calculadora():
+        print(f"5 + 3 ={calc.soma(5,3,)}")
+        print(f"5*3 ={calc.multiplica(5,3)}")
+        print(f"5/5 = {calc.divisão(5,5)}")
+        print(f"10 - 9 = {calc.subtrai( 10,9)}")
+
+
+
+if __name__ == "__main__":
+    testa_calculadora()
